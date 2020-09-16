2794af84a080c16cadc549cf80acf99afc414d35
badburrito24 <59742387+badburrito24@users.noreply.github.com>
2020-09-16 11:42:43 -0500

    Add files via upload

--- ButtonFlash.java
+++ ButtonFlash.java
@@ -6,6 +6,7 @@ public class ButtonFlash {                    //this is not to be confused with 
 
     private double x;
     private double y;
+    private int z;
     GameWindow g;
     private BufferedImage abutton2; //sprite for white a button
     private BufferedImage bbutton2; //sprite for b button
@@ -23,6 +24,14 @@ public class ButtonFlash {                    //this is not to be confused with 
 	private BufferedImage stickULeft2;
 	private BufferedImage stickDRight2;
 	private BufferedImage stickDLeft2;
+    private BufferedImage cUp;
+    private BufferedImage cDown;
+    private BufferedImage cLeft;
+    private BufferedImage cRight;
+    private BufferedImage cUR;
+    private BufferedImage cUL;
+    private BufferedImage cDR;
+    private BufferedImage cDL;
 
 
     public ButtonFlash(double x, double y, GameWindow g) {
@@ -46,6 +55,16 @@ public class ButtonFlash {                    //this is not to be confused with 
 		stickULeft2 = ss.grabImage(2, 6, 32, 32);
 		stickDRight2 = ss.grabImage(6, 5, 32, 32);
 		stickDLeft2 = ss.grabImage(4, 5, 32, 32);
+		//c stick
+        cDown = ss.grabImage(4, 6, 32, 32);
+        cUp = ss.grabImage(7, 6, 32, 32);
+        cLeft = ss.grabImage(1, 7, 32, 32);
+        cRight = ss.grabImage(2, 7, 32, 32);
+        //c stick diagonals
+        cUR = ss.grabImage(8, 6, 32, 32);
+        cUL = ss.grabImage(6, 6, 32, 32);
+        cDL = ss.grabImage(3, 6, 32, 32);
+        cDR = ss.grabImage(5, 6, 32, 32);
 
     }
 
@@ -114,6 +133,50 @@ public class ButtonFlash {                    //this is not to be confused with 
         if (e.getKeyCode() == KeyEvent.VK_4) {
             x = 247;
             y = 390;
+        }
+      //c stick (needs a z coord simply because there are so many sprites going into 390, 440)
+        // maybe, if one key is pressed, move two sprites?
+        if (e.getKeyCode() == KeyEvent.VK_U) {
+            x = 390;
+            y = 440;
+            z=1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_H) {
+        	 x = 390;
+             y = 440;
+             z= 2;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_J) {
+        	 x = 390;
+             y = 440;
+             z = 3;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_K) {
+        	 x = 390;
+             y = 440;
+             z = 4;
+        }
+        
+        // c stick diagonal inputs
+        if (e.getKeyCode() == KeyEvent.VK_7) {
+        	x = 390;
+        	y = 440;
+            z = 5;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_I) {
+        	x = 390;
+        	y = 440;
+            z = 6;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_N) {
+        	x = 390;
+        	y = 440;
+            z = 7;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_M) {
+        	x = 390;
+        	y = 440;
+            z = 8;
         }
 
     }
@@ -198,6 +261,51 @@ public class ButtonFlash {                    //this is not to be confused with 
             x = -100;
             y = -100;
         }
+      //c stick diagonals: // try an if statement within an if statement? instead of the and?
+        if (e.getKeyCode() == KeyEvent.VK_7) {
+            x = -100;
+            y = -100;
+            z=0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_I) {
+            x = -100;
+            y = -100;
+            z=0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_N) {
+            x = -100;
+            y = -100;
+            z=0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_M) {
+            x = -100;
+            y = -100;
+            z=0;
+        }
+        
+        
+        // c stick
+        if (e.getKeyCode() == KeyEvent.VK_U) {
+            x = -100;
+            y = -100;
+            z=0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_H) {
+        	 x = -100;
+             y = -100;
+             z= 0;
+        }
+        
+        if (e.getKeyCode() == KeyEvent.VK_J) {
+        	 x = -100;
+             y = -100;
+             z = 0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_K) {
+        	 x = -100;
+             y = -100;
+             z = 0;
+        }
     }
 
     public void tick() //update method
@@ -223,6 +331,9 @@ public class ButtonFlash {                    //this is not to be confused with 
         }
         if (x == 250 && y == 340) {
             g.drawImage(lbutton2, (int) x, (int) y, null);
+        }
+        if (x == 300 && y == 420) {
+            g.drawImage(startbutton2, (int) x, (int) y, null);
         }
         if (x == 405 && y == 340) {
             g.drawImage(rbutton2, (int) x, (int) y, null);
@@ -260,6 +371,31 @@ public class ButtonFlash {                    //this is not to be confused with 
         if (x == 390 && y == 350) {
             g.drawImage(zbutton2, (int) x, (int) y, null);
         }
+        if (x == 390 && y == 440 && z == 1) {
+            g.drawImage(cUp, (int) x, (int) y, null);
+        }
+        if (x == 390 && y == 440 && z == 2) {
+            g.drawImage(cLeft, (int) x, (int) y, null);
+        }
+        if (x == 390 && y == 440 && z == 3) {
+            g.drawImage(cDown, (int) x, (int) y, null);
+        }
+        if (x == 390 && y == 440 && z == 4) {
+            g.drawImage(cRight, (int) x, (int) y, null);
+        }
+        if (x == 390 && y == 440 && z == 5) {
+            g.drawImage(cUL, (int) x, (int) y, null);
+        }
+        if (x == 390 && y == 440 && z == 6) {
+            g.drawImage(cUR, (int) x, (int) y, null);
+        }
+        if (x == 390 && y == 440 && z == 7) {
+            g.drawImage(cDL, (int) x, (int) y, null);
+        }
+        if (x == 390 && y == 440 && z == 8) {
+            g.drawImage(cDR, (int) x, (int) y, null);
+        }
+    
 
 
     }
