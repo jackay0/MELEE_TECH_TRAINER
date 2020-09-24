0c0b3bd939ea18d0816fdb5698d406f08637af1c
badburrito24 <59742387+badburrito24@users.noreply.github.com>
2020-09-23 22:48:47 -0500

    Add files via upload

--- Note.java
+++ Note.java
@@ -1,9 +1,11 @@
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-public class Note {
+import com.game.src.main.classes.EntityA;
 
-private double x;
-private double y;
+public class Note extends GameObject implements EntityA {
+
+
 private String type;
 GameWindow g;
 BufferedImage a;
@@ -15,9 +17,9 @@ BufferedImage a;
 public Note(int x, int y, GameWindow g) 
 {
 	
-	this.x = x;
+	super(x,y);
 	this.g = g;
-	this.y = y;
+	
 	//this.type = type;
 	SpriteSheet z = new SpriteSheet(g.getSpriteSheet());
 	a = z.grabImage(1, 1, 32, 32);
@@ -29,7 +31,7 @@ public Note(int x, int y, GameWindow g)
 public void tick() //update method
 {
 	y++;
-
+	 //note is entity a, checks to see if collision is true for colliding with a buttonflash
 }
 
 
@@ -39,6 +41,30 @@ public void render(Graphics g)  //draws out image
 	g.drawImage(a, (int) x, (int) y, null);
 
 
+}
+
+
+
+@Override
+public double getX() {
+	// TODO Auto-generated method stub
+	return x;
+}
+
+
+
+@Override
+public double getY() {
+	// TODO Auto-generated method stub
+	return y;
+}
+
+
+
+
+public Rectangle getBounds() {
+	
+	return new Rectangle((int)x,(int)y, 32, 32);
 }
 
 
